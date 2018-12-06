fa26cbcb46133c3018ea5e45fe15d498d4e1a0b8
Mahesh-wor <maheshwor89@gmail.com>
2018-12-06 23:36:31 +0545

    stripped + models

--- dealsnepal/dealsnepalapp/models.py
+++ dealsnepal/dealsnepalapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class smartphone(models.Model):
+    name= models.CharField(max_length=120)
+    price= models.IntegerField()
+    link=models.URLField()
+
+    def __self__(self):
+        return self.name
+
